cacbda34b130200e92e60bea498e03278bcdec95
shoaibgitpractice <shoaibnadafg@gmail.com>
2018-11-10 07:30:23 +0000

    Student02nov class program file created for test.java

---
+++ Student02nov.java
@@ -0,0 +1,18 @@
+class Student02nov 
+{
+	static String PVPIT="PVPIT";
+
+	int sno;
+	String sname;
+	static void PVPIT(){
+        
+		System.out.println(PVPIT + "institute");
+	}
+}
+
+
+//output
+//D:\java\methods>javac College02nov.java
+
+//D:\java\methods>java College02nov
+//PVPITinstitute//outout
