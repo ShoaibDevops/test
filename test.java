ab1e135356d3ec03f4e2b5e15198a5bb85944088
shoaibgitpractice <shoaibnadafg@gmail.com>
2018-11-10 07:17:11 +0000

    first java program is added

---
+++ test.java
@@ -0,0 +1,20 @@
+
+
+//calliing static variable from another class 
+class test
+{
+	public static void main(String[] args) 
+	{
+        
+		Student02nov.PVPIT();  //called by its class name where PVPIT variable is present because its static method 
+		
+	}
+}
+
+
+//output
+//D:\java\methods>javac test.java
+
+//D:\java\methods>java test
+//PVPITinstitute
+
